a197e5b22a8c309f1e0eaddddc91a79f85f4d5a7
Param Goswami <paramgoswami2000@gmail.com>
2020-07-09 00:12:06 +0530

    Commit for 08-07-2020

---
+++ 379-A： New Year Candles.cpp
@@ -0,0 +1,59 @@
+//https://codeforces.com/contest/379/problem/A
+
+#pragma GCC optimize ("-O3")
+#include<bits/stdc++.h>
+#include<cmath>
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+ll NUM = 1e9+7;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define ff first
+#define ss second
+#define INF 1e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define zer ll(0)
+
+void solve()
+{
+  int a,b,ans=0;
+  cin>>a>>b;
+  int curr=a,die=0;
+  while(curr>0)
+  {
+    curr--;
+    die++;
+    ans++;
+    if(die%b==0)curr++;
+  }
+  cout<<ans;
+}
+
+int  main()
+{
+  fast_cin();
+  ll t=1;
+  //cin>>t;
+  while(t--)solve();
+  return 0;
+} 
